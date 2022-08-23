0e26f968c3e8395b7a393f3d66259e69fe36c31b
madrid6223 <karakas@ukr.net>
2022-08-23 14:27:28 +0300

    test_first_api

--- test_first_api.py
+++ test_first_api.py
@@ -1,18 +1,30 @@
+import pytest # for @pytest.mark.parametrize
 import requests
 
 class TestFirstApi:
-    def test_hello_call(selfself):
+    names = [
+        ("Vovo"),
+        ("Marynka"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url="https://playground.learnqa.ru/api/hello"
-        name = 'Vovo'
+        #name = 'Vovo'
         data = {'name':name}
 
-        response = requests.get(url)
+        response = requests.get(url, params=data)
 
         assert response.status_code ==200, "Ошибка кода ответа"
 
         response_dict=response.json()
         assert "answer" in response_dict, "Поля 'answer' нет а response"
 
-        expected_response_text=f"Hello, {name}"
+        if len(name) ==0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text=f"Hello, {name}"
+
         actual_response_text=response_dict["answer"]
         assert actual_response_text==expected_response_text, "Сравнение не прошло"
