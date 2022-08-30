6f1a00a0f2611418081bf57882fc771a01ee7772
madrid6223 <karakas@ukr.net>
2022-08-30 10:01:20 +0300

    add TestUserRegister

--- tests/test_user_registry.py
+++ tests/test_user_registry.py
@@ -2,3 +2,17 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+class TestUserRegister(BaseCase):
+    def test_create_user_with_existing_email(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa'
+            'lastName': 'learnqa'
+            'email': 'vinkotov@example.com'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        print(response.status_code)
+        print(response.content)
