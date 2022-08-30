59260f323e7daec6dda5a8bac1ac437ecd9806a3
madrid6223 <karakas@ukr.net>
2022-08-30 10:15:45 +0300

    add TestUserRegister

--- tests/test_user_registry.py
+++ tests/test_user_registry.py
@@ -4,15 +4,18 @@ from lib.assertions import Assertions
 
 class TestUserRegister(BaseCase):
     def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
         data = {
             'password': '123',
             'username': 'learnqa',
-            'firstName': 'learnqa'
-            'lastName': 'learnqa'
-            'email': 'vinkotov@example.com'
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
         }
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        #print(response.status_code)
+        #print(response.content)
 
-        print(response.status_code)
-        print(response.content)
+        assert response.status_code == 400, f"Неочікуваний status_code{response.status_code}"
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
